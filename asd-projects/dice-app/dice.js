f0e29b8c6c23c4023cbd0ce85f8e6024132ce1f6
KingIsCoolerXD <kingston.operationspark@gmail.com>
2026-06-05 14:46:22 +0000

    Something really really cool

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -8,20 +8,44 @@ $(document).ready(function () {
   .css("width", 15)
   .css("background-color", "black")
   .css("position", "absolute")
-  .css("top", 50)
-  .css("left", 50)
-  .appendTo("#die");
+  .css("top", top)
+  .css("left", left)
+  .appendTo(elementID);
 
   }
 
-  makeDot(50, 50, "#die");
-  makeDot(25, 25, "#die");
-  makeDot(75, 75, "#die");
-
   function rollDie(dieID){
+    $(dieID).empty();
     var randomNum = Math.ceil(Math.random() * 6);
     console.log(randomNum);
-    
+    if (randomNum === 1) {
+  makeDot(50, 50, dieID); // middle middle
+} else if (randomNum === 2) {
+  makeDot(25, 25, dieID); // top left
+  makeDot(75, 75, dieID); // bottom right
+} else if (randomNum === 3) {
+  makeDot(25, 25, dieID); // top left
+  makeDot(75, 75, dieID); // bottom right
+  makeDot(50, 50, dieID); // middle middle
+} else if (randomNum === 4) {
+  makeDot(75, 75, dieID); // bottom right
+  makeDot(25, 25, dieID); // top left
+  makeDot(25, 75, dieID); // bottom left
+  makeDot(75, 25, dieID); // top right
+} else if (randomNum === 5) {
+  makeDot(50, 50, dieID); // middle middle
+  makeDot(75, 75, dieID); // bottom right
+  makeDot(25, 25, dieID); // top left
+  makeDot(25, 75, dieID); // bottom left
+  makeDot(75, 25, dieID); // top right
+} else if (randomNum === 6){
+  makeDot(75, 75, dieID); // bottom right
+  makeDot(25, 25, dieID); // top left
+  makeDot(25, 75, dieID); // bottom left
+  makeDot(75, 25, dieID); // top right
+  makeDot(50, 75, dieID)
+  makeDot(50, 25, dieID)
+}
 
   }
 
